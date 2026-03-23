18c4c6da53d6ed1fe29e50053722e08666d97020
Jan Schmidt <hi@jnsmdt.dev>
2026-03-23 21:51:33 +0100

    feat: add rand function

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from equations-showcase!")
+# ⚠️ This Files uses Jupyter in File Cells instead of a real Notebook
+
+# %% Imports
+import random
+
+import numpy as np
+
+# import numpy as np
+from matplotlib import pyplot as plt
+
+# %% Equations
 
 
-if __name__ == "__main__":
-    main()
+# random Value
+def true_random(min=0, max=100, step=1):
+    return random.SystemRandom().choice(range(min, max, step))
+
+
+# Gaussion Normal Distribution
+
+
+# %% Plot
+##### True Random Histogram
+result_true_random = []
+true_random_x = range(0, 10000)
+for x in true_random_x:
+    result_true_random.append(true_random())
+
+fig, ax = plt.subplots()
+
+ax.hist(result_true_random, bins=8, linewidth=0.5, edgecolor="white")
+
+plt.show()
